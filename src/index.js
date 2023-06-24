c1f5e7ab9f1f5b38c1022816c32f21a87d3003ae
Felix Wilson Gbedemah <afrogbede09@gmail.com>
2023-06-24 06:25:18 -0700

    new updates and chnages

---
+++ src/index.js
@@ -0,0 +1,43 @@
+
+  
+  
+/* Footer Current year script */
+const year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+
+/* Image Caurosel script */
+document.addEventListener("DOMContentLoaded", function() {
+    let slideIndex = 0;
+    showSlides();
+  
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].className += " active";
+      setTimeout(showSlides, 5000); // Change image every 5 seconds
+    }
+  });
+  
+
+/**Harmburger menu JS**/
+function toggleMenu(){ 
+    document.getElementById('primaryNav').classList.toggle("open");
+    document.getElementById('hamburgerBtn').classList.toggle("open")
+    }
+    
+    const x = document.getElementById('hamburgerBtn')
+    x.onclick = toggleMenu
+
+
