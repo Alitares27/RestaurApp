df0e02010c7a4e076bbc6ec87ae570d3bf95a1b5
Aldair Payehuanca <aldair2795@hotmail.com>
2023-07-05 23:53:38 -0500

    styling

--- src/js/test.js
+++ src/js/test.js
@@ -1,101 +1,125 @@
-const productContainer = document.querySelector('.productContainer')
-const previous = document.querySelector('#previous')
-const next = document.querySelector('#next')
-let offset = 1
-let limit = 3
+const productContainer = document.querySelector(".productContainer");
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+let offset = 1;
+let limit = 3;
 
-previous.addEventListener('click', () => {
-    if (offset != 1) {
-        offset -= 4
-        removeChildNodes(productContainer)
-        fetchProducts(offset, limit)
-    }})
-next.addEventListener('click', () => {
+previous.addEventListener("click", () => {
+  if (offset != 1) {
+    offset -= 4;
+    removeChildNodes(productContainer);
+    fetchProducts(offset, limit);
+  }
+});
+next.addEventListener("click", () => {
+  if (offset < 16) {
+    offset += 4;
+    removeChildNodes(productContainer);
+    fetchProducts(offset, limit);
+  }
+});
+function fetchStore(id) {
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => res.json())
+    .then((json) => {
+      createProduct(json);
+    });
+}
+function fetchProducts(offset, limit) {
+  for (let i = offset; i <= offset + limit; i++) {
+    fetchStore(i);
+  }
+}
 
-        offset += 4
-        removeChildNodes(productContainer)
-        fetchProducts(offset, limit)
+function one() {
+  const opt1 = document.getElementById("option1");
+  const option1 = opt1.value;
+  productContainer.innerHTML = "";
+  getCategory(option1);
+}
+one();
 
-    })
-    function fetchStore(id) {
-        fetch(`https://fakestoreapi.com/products/${id}`)
-            .then(res => res.json())
-            .then(json => {createProduct(json)})
-    }
-    function fetchProducts(offset, limit) {
-        for (let i = offset; i <= offset + limit; i++) {
-            fetchStore(i)
-        }
-    }
+function two() {
+    const opt2 = document.getElementById("option2");
+const option2 = opt2.value;
+    productContainer.innerHTML = "";
+    getCategory(option2);
+  }
+  two();
 
-    function getElectronics(){
-        fetch(`https://fakestoreapi.com/products/category/electronics`)
-        .then(res=>res.json())
-        .then(json=>console.log(json))
-    }    
-    function getJewelery(){
-        fetch(`https://fakestoreapi.com/products/category/jewelery`)
-        .then(res=>res.json())
-        .then(json=>console.log(json))
-    }
-    function getMen(){
-        fetch(`https://fakestoreapi.com/products/category/men's%20clothing`)
-        .then(res=>res.json())
-        .then(json=>console.log(json))
-    }
-    function getWomen(){
-        fetch(`https://fakestoreapi.com/products/category/women's%20clothing`)
-        .then(res=>res.json())
-        .then(json=>console.log(json))
-    }
+  function three() {
+    const opt3 = document.getElementById("option3");
+    const option3 = opt3.value;
+    productContainer.innerHTML = "";
+    getCategory(option3);
+  }
+  three();
 
-    function createProduct(products) {
-        const card = document.createElement('div')
-        card.classList.add('productBlock')
+  function four() {
+    const opt4 = document.getElementById("option4");
+    const option4 = opt4.value;
+    productContainer.innerHTML = "";
+    getCategory(option4);
+  }
+  four();
 
-        const imageContainer = document.createElement('div')
-        imageContainer.classList.add('imgContainer')
+function getCategory(category) {
+  fetch('https://fakestoreapi.com/products/category/'+ category)
+    .then((res) => res.json())
+    .then((json) => {
+      createProduct(json);
+    });
+  /*.then(json=>console.log(json))*/
+}
+getCategory();
 
-        const image = document.createElement('img')
-        image.classList.add('itemImage')
-        image.src = products.image
+function createProduct(products) {
+  const card = document.createElement("div");
+  card.classList.add("productBlock");
 
-        imageContainer.appendChild(image)
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("imgContainer");
 
-        const number = document.createElement('p')
-        number.classList.add('sku')
-        number.textContent = 'SKU: 0100' + products.id
+  const image = document.createElement("img");
+  image.classList.add("itemImage");
+  image.src = products.image;
 
-        const name = document.createElement('h1')
-        name.classList.add('name')
-        name.textContent = products.title
+  imageContainer.appendChild(image);
 
-        const description = document.createElement('p')
-        description.classList.add('description')
-        description.textContent = products.description
+  const number = document.createElement("p");
+  number.classList.add("sku");
+  number.textContent = "SKU: 0100" + products.id;
 
-        const price = document.createElement('h3')
-        price.classList.add('price')
-        price.textContent = '$ ' + products.price
+  const name = document.createElement("h1");
+  name.classList.add("name");
+  name.textContent = products.title;
 
-        const addCart = document.createElement('button')
-        addCart.classList.add('addCart')
-        addCart.textContent = 'Add to Cart'
+  const description = document.createElement("p");
+  description.classList.add("description");
+  description.textContent = products.description;
 
-        card.appendChild(number)
-        card.appendChild(name)
-        card.appendChild(imageContainer)
-        card.appendChild(description)
-        card.appendChild(price)
-        card.appendChild(addCart)
+  const price = document.createElement("h3");
+  price.classList.add("price");
+  price.textContent = "$ " + products.price;
 
-        productContainer.appendChild(card)
-    }
+  const addCart = document.createElement("button");
+  addCart.classList.add("addCart");
+  addCart.textContent = "Add to Cart";
 
-    function removeChildNodes(parent) {
-        while (parent.firstChild) {
-            parent.removeChild(parent.firstChild);
-        }
-    }
+  card.appendChild(number);
+  card.appendChild(name);
+  card.appendChild(imageContainer);
+  card.appendChild(description);
+  card.appendChild(price);
+  card.appendChild(addCart);
 
-    fetchProducts(offset, limit)
+  productContainer.appendChild(card);
+}
+
+function removeChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+fetchProducts(offset, limit);
